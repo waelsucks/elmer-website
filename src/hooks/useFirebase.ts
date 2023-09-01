6f55cf89cbf8198421f800c5b14c2bc8eef90bb4
Wael Mahrous <waelbmahrous@gmail.com>
2023-09-01 18:41:24 +0200

    Test.

--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -2,15 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
-
-};
+    apiKey: "AIzaSyAiNQ1MntNCA79rEhJCvQKl4JV-sqUQw7Q",
+    authDomain: "elmer-site.firebaseapp.com",
+    projectId: "elmer-site",
+    storageBucket: "elmer-site.appspot.com",
+    messagingSenderId: "995427603745",
+    appId: "1:995427603745:web:5270cb75bdcae77c775a60"
+  };
 
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app)
